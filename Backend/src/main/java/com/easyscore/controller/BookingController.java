2cb77ef05fbc1777783d00326dc9badafe1353c5
Julian Maccor <maccorjulian@gmail.com>
2024-06-19 12:28:06 -0300

    Railway no funciona en java 22 + Booking controller terminado a espera de test

--- Backend/src/main/java/com/easyscore/controller/BookingController.java
+++ Backend/src/main/java/com/easyscore/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.easyscore.controller;
 
 import com.easyscore.model.Booking;
 import com.easyscore.service.BookingService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,13 @@ public class BookingController {
     @Autowired
     private BookingService bookingService;
 
+    @Operation(summary = "Lista todas las reservas")
     @GetMapping
     public List<Booking> getAllBookings() {
         return bookingService.findAll();
     }
 
+    @Operation(summary = "Crea una nueva reserva")
     @PostMapping
     public ResponseEntity<String> createBooking(@RequestBody Booking booking) {
         LocalTime startTime = booking.getHoraInicio();
@@ -36,6 +39,7 @@ public class BookingController {
         }
     }
 
+    @Operation(summary = "Busca reservas por producto y fecha")
     @GetMapping("/product/{productId}/date/{date}")
     public List<Booking> getBookingsByProductAndDate(@PathVariable Long productId, @PathVariable String date) {
         LocalDate localDate = LocalDate.parse(date);
